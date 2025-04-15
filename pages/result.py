29273b32da683084a81325d76795b0f69140dd53
syeedf <syeed.farrakhan@bison.howard.edu>
2025-04-15 19:33:14 -0400

    Added the locators for the search bar and search result title. completed the load method to create the chrome instance. Added the URL

--- pages/result.py
+++ pages/result.py
@@ -1,7 +1,11 @@
 ''' This module contains DDGOResult page
 '''
+from selenium.webdriver.common.by import By
 
 class DuckDuckGoResultPage: 
+
+    result_links = (By.CSS_SELECTOR,"[data-testid='result-title-a']")
+    search_input= (By.id, 'searchbox_input')
     def __init__(self, browser):
         self.browser = browser
     
--- pages/search.py
+++ pages/search.py
@@ -2,14 +2,20 @@
 This module contains DDGo Search Page, the page object for the ddgo seatch page
 '''
 
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
 class DuckDuckGoSearchPage:
+
+    url= 'https://www.duckduckgo.com'
+    search_input= (By.id, 'searchbox_input')
+    
     def __init__(self, browser):
         self.browser=browser
+    
     def load(self):
+        self.browser.get(self.url)
         
-        #TODO
-        
-        pass
     def search(self, phrase):
         #TODO
         pass
