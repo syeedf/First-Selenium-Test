96c9d809e593fdb139602889a5f74ff55da90cb5
syeedf <syeed.farrakhan@bison.howard.edu>
2025-05-17 15:41:09 -0400

    Removed the sleep that reduced the time by 13percent and added an implicit wait for better time.

--- pages/search.py
+++ pages/search.py
@@ -5,7 +5,6 @@ This module contains DDGo Search Page, the page object for the ddgo seatch page
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
-
 class DuckDuckGoSearchPage:
 
     URL = 'https://www.duckduckgo.com'
@@ -18,10 +17,9 @@ class DuckDuckGoSearchPage:
         self.browser.get(self.URL)
         
     def search(self, phrase):
-        time.sleep(2)
+  
         searchInput = self.browser.find_element(*self.SEARCH_INPUT)
         print('found the search box')
         searchInput.send_keys(phrase+Keys.RETURN)
-        time.sleep(2)
-        
+    
         print('searching for panda')
--- tests/test_search.py
+++ tests/test_search.py
@@ -4,25 +4,30 @@ these test cover duckduckgo searches
 """
 from pages.result import DuckDuckGoResultPage
 from pages.search import DuckDuckGoSearchPage
+import pytest
 
-def test_basic_duckduckgo_search(browser):
+@pytest.mark.parametrize('phrase', ['panda', 'python', 'polar bear'])
+def test_basic_duckduckgo_search(browser,phrase):
 
     search_page=DuckDuckGoSearchPage(browser)
     result_page=DuckDuckGoResultPage(browser)
-    phrase= "panda"
+
     
     #Given the duckduckgo home page is displayed
     search_page.load()
     #When the user searches for "panda"
     search_page.search(phrase)
 
-    #Then the search result title contains "panda"
-    assert phrase in result_page.title()
-
     #And the search result query is "panda"
     assert phrase== result_page.search_input_value()
+
 
     #And the search result links pertain to "panda"
     for title in result_page.result_link_titles():
         assert phrase.lower() in title.lower()
 
+
+    #Then the search result title contains "panda"
+    browser.implicitly_wait(2)
+    assert phrase in result_page.title()
+    
